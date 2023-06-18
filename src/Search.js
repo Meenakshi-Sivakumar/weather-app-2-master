6c4d4a3e73b769f188507342478132bc3ff4c697
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-06-18 10:43:00 +0530

    4

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
-import { BsSearch } from 'react-icons/bs';
+import { BsSearch } from "react-icons/bs";
 
 const Search = ({ handleCityUpdate }) => {
-  const [cityInput, setCityInput] = useState('');
+  const [cityInput, setCityInput] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(cityInput === '') {
-      alert("enter a city name");
+    if (cityInput === "") {
+      alert("Enter a city name");
       return;
     }
     handleCityUpdate(cityInput);
-    setCityInput('');
-  }
+    setCityInput("");
+  };
 
   return (
-    <div className='search-container'>
+    <div className="search-container">
       <div className="search-box">
         <form onSubmit={handleSubmit}>
           <input
@@ -24,18 +24,30 @@ const Search = ({ handleCityUpdate }) => {
             value={cityInput}
             onChange={(e) => setCityInput(e.target.value)}
           />
-          <button type="submit"><BsSearch className="react-search-icon"/></button>
+          <button type="submit">
+            <BsSearch className="react-search-icon" />
+          </button>
         </form>
       </div>
       <ul className="popular-cities-list">
-        <li><button onClick={() => handleCityUpdate('Salem')}>Salem</button></li>
-        <li><button onClick={() => handleCityUpdate('Coimbatore')}>Coimbatore</button></li>
-        <li><button onClick={() => handleCityUpdate('Chennai')}>Chennai</button></li>
-        <li><button onClick={() => handleCityUpdate('London')}>London</button></li>
+        <li>
+          <button onClick={() => handleCityUpdate("Salem")}>Salem</button>
+        </li>
+        <li>
+          <button onClick={() => handleCityUpdate("Coimbatore")}>
+            Coimbatore
+          </button>
+        </li>
+        <li>
+          <button onClick={() => handleCityUpdate("Chennai")}>Chennai</button>
+        </li>
+        <li>
+          <button onClick={() => handleCityUpdate("London")}>London</button>
+        </li>
       </ul>
       <div className="divider"></div>
     </div>
   );
-}
+};
 
 export default Search;
