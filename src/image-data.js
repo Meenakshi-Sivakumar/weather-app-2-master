2644cf8e8dcbd114936482e4c70fbdc6def964dd
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-06-18 11:11:13 +0530

    3

--- src/image-data.js
+++ src/image-data.js
@@ -5,7 +5,6 @@ const images = [
     'https://images.unsplash.com/photo-1457264635001-828d0cbd483e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80',
     'https://images.unsplash.com/photo-1596721044298-2306f0405d03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
     'https://images.unsplash.com/photo-1494694081617-38b4cba8fc36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-    'https://images.unsplash.com/photo-1506069748786-1f7a3d5d54cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80',
     'https://images.unsplash.com/photo-1662298594573-c3b80e67f749?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80',
     'https://images.unsplash.com/photo-1525807609479-05c325ccadd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80'
 ];
